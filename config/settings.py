f4b1b6baf44df8c00bbd0a735d2240d7f0c174a5
franckdigital <franckalain.digital@gmail.com>
2026-06-19 23:21:34 +0000

    first commit

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME', default='campus'),
         'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
+        'PASSWORD': config('DB_PASSWORD', default='xamil@IFE2025'),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='3306'),
         'OPTIONS': {
